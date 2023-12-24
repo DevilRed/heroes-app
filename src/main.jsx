9948184b78388a4a2ff199466c60f8322fc7a521
Christian <chrisdaq@gmail.com>
2023-12-24 17:25:08 -0400

    update import of pages to make file more clean

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
-import { HeroesApp } from "./HeroesApp";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AppRouter from "./routes/AppRouter";
-import { MarvelPage } from "./heroes/pages/MarvelPage";
-import { DcPage } from "./heroes/pages/DcPage";
-import { LoginPage } from "./auth/pages/LoginPage";
-import { Children } from "react";
+import { MarvelPage, DcPage } from "./heroes";
+import { LoginPage } from "./auth";
 
 const router = createBrowserRouter([
   {
