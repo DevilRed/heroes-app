a1d381ed07378cd01d03889f63666d4a27f8c57d
Christian <chrisdaq@gmail.com>
2023-12-24 17:21:22 -0400

    load Navbar on AppRouter

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,40 +1,10 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { Navbar } from "../ui";
 
 export default function AppRouter() {
   return (
     <>
-      <div id="sidebar">
-        <h1>Heroes App</h1>
-        <div>
-          <form id="search-form" role="search">
-            <input
-              id="q"
-              aria-label="Search contacts"
-              placeholder="Search"
-              type="search"
-              name="q"
-            />
-            <div id="search-spinner" aria-hidden hidden={true} />
-            <div className="sr-only" aria-live="polite"></div>
-          </form>
-          <form method="post">
-            <button type="submit">New</button>
-          </form>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <Link to={`marvel`}>Marvel</Link>
-            </li>
-            <li>
-              <Link to={`dc`}>DC</Link>
-            </li>
-            <li>
-              <Link to={`login`}>Login</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <Navbar />
       <div id="detail">
         <Outlet />
       </div>
