16d5deee7808aa563fe32570e43c94f3863371b5
Christian <chrisdaq@gmail.com>
2023-12-25 11:35:49 -0400

    move routes definition from main.jsx to HeroesApp.jsx

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,3 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { MarvelPage, DcPage } from "./heroes";
+import AppLayout from "./layouts/AppLayout";
+import { LoginPage } from "./auth";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "marvel",
+        element: <MarvelPage />,
+      },
+      {
+        path: "dc",
+        element: <DcPage />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+    ],
+  },
+]);
+
 export const HeroesApp = () => {
-  return <div>HeroesApp</div>;
+  return (
+    <RouterProvider router={router}>
+      <div>HeroesApp</div>
+    </RouterProvider>
+  );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import AppRouter from "./routes/AppRouter";
-import { MarvelPage, DcPage } from "./heroes";
-import { LoginPage } from "./auth";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppRouter />,
-    children: [
-      {
-        path: "marvel",
-        element: <MarvelPage />,
-      },
-      {
-        path: "dc",
-        element: <DcPage />,
-      },
-      {
-        path: "login",
-        element: <LoginPage />,
-      },
-    ],
-  },
-]);
+import { HeroesApp } from "./HeroesApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HeroesApp />
   </React.StrictMode>
 );
