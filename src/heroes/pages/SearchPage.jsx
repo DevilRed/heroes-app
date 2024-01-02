7261dd34bf9e70f9ce6d632b6ea5edb8efe6bb68
Christian <chrisdaq@gmail.com>
2024-01-02 17:04:39 -0400

    use helper on searchPage which is loading a HeroCard

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,3 +1,58 @@
+import { HeroCard } from "../components";
+import { useForm } from "../../hooks/useForm";
+import { useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+import { getHeroesByName } from "../helpers/getHeroesByName";
+
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const navigate = useNavigate();
+  const location = useLocation();
+  // console.log(location);
+
+  const { q = "" } = queryString.parse(location.search);
+  const heroes = getHeroesByName(q);
+
+  const { searchText, onInputChange } = useForm({
+    searchText: q,
+  });
+
+  const onSearchSubmit = (ev) => {
+    ev.preventDefault();
+    if (searchText.trim().length <= 1) return;
+
+    // set param to current location
+    navigate(`?q=${searchText}`);
+    //}
+  };
+  return (
+    <>
+      <h1>Search</h1>
+      <hr />
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+          <form onSubmit={onSearchSubmit}>
+            <input
+              type="text"
+              name="searchText"
+              value={searchText}
+              onChange={onInputChange}
+            />
+            <button className="btn btn-outline-primary mt-1">Search</button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+          <div className="alert alert-primary">Search a hero</div>
+          <div className="alert alert-danger">
+            No hero with <b>{q}</b>
+          </div>
+
+          {heroes && heroes.map((hero) => <HeroCard key={hero.id} {...hero} />)}
+        </div>
+      </div>
+    </>
+  );
 };
